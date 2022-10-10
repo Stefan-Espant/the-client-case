8a41995446d08f5956b0484a75ee12c38ec073c1
Stefan Espant <info@espant.com>
2022-10-10 22:04:05 +0200

    v3.0
    Content is aan de pagina's toegevoegd. Knoppen hebben een collapse functie waarmee meerdere paragrafen tekst weergegeven kunnen worden.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function toggle(id) {
+    var showCtnt = document.getElementById(id);
+    if(showCtnt.style.display == "none") showCtnt.style.display = "block";
+
+    else 
+        showCtnt.style.display = "none";   
+}
